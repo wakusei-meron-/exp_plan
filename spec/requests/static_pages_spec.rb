2830a4cd344725d789ce2f3dc8974d770544250f
Genki Ishibashi <b0941015@gmail.com>
2014-08-13 18:47:22 +0900

    Add guard

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,27 +4,27 @@ describe "StaticPages" do
 
   describe "Home page" do
     it "shoule have the content 'Exp plan')" do
-      visit "/static_pages/home"
+      visit "/home"
       expect(page).to have_content('Home')
     end
   end
 
   describe "howToUse page" do
     it "shoule have the content 'Exp plan')" do
-      visit "/static_pages/howtouse"
+      visit "/howtouse"
       expect(page).to have_content('How to use')
     end
   end
 
   describe "About page" do
     it "shoule have the content 'Exp plan')" do
-      visit "/static_pages/about"
+      visit "/about"
       expect(page).to have_content('About')
     end
   end
   describe "Contact page" do
     it "shoule have the content 'Exp plan')" do
-      visit "/static_pages/contact"
+      visit "/contact"
       expect(page).to have_content('Contact')
     end
   end
